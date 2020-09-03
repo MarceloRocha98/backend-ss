8ed964867cb3bb34352a29816b43f4cfc5bea394
MarceloRochaDev <marceloteixeirar2014@gmail.com>
2020-09-02 22:21:36 -0300

    criada a tabela profile

--- migrations/20200902221147_create_table_profile.js
+++ migrations/20200902221147_create_table_profile.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('profile', table => {
         table.increments('id').primary()
-        table.string('userId').references('id').inTable('users').notNull()
+        table.integer('userId').references('id').inTable('users').notNull()
         table.string('aboutMe', 1000)
         table.integer('avaliationsPoints')
         table.integer('totalAvaliations')
